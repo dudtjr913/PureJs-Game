0879480a3d0aa8d795e60248443184af21a3cf3d
unknown <dudtjr913@naver.com>
2020-11-03 22:59:59 +0900

    update : 끝말잇기 4번 완료 <description> 끝 단어가 일치할 경우 정답처리, 일치하지 않을 경우 목숨을 줄이는 알고리즘을 작성했는데, 목숨 자체를 게임 시작할 때 변수로 할당해두고, 인풋을 받았을 때, 인풋이 올바르지 않으면 1씩 깎는 것으로 했다. 맨 처음에 의도했던 것은 정답을 틀렸을 때만 이 변수를 받아서 점수를 깎는 것으로 하려고 했는데, 전역변수를 사용하지 않으려고 하다보니까 파라미터로 넘어가게 되었다. 맞는 코드인지 정말 궁금하다. <issue-number>

--- WordRelay/WordRelay.js
+++ WordRelay/WordRelay.js
@@ -5,12 +5,14 @@ const makeScreen = () => {
   const form = document.createElement("form");
   const userInput = document.createElement("input");
   const life = document.createElement("div");
+  const answer = document.createElement("div");
   title.innerHTML = "<h1>끝말잇기</h1>";
   wordTitle.innerText = "단어 : ";
   firstWord.classList.add("word");
   firstWord.innerText = "프론트엔드";
   userInput.placeholder = "단어를 입력해주세요.";
   userInput.type = "text";
+  life.classList.add("life");
   life.innerText = "목숨 : 5";
   document.body.append(title);
   title.append(wordTitle);
@@ -18,30 +20,45 @@ const makeScreen = () => {
   document.body.append(form);
   form.append(userInput);
   document.body.append(life);
+  answer.classList.add("answer");
+  document.body.append(answer);
 };
 
-const rightAnswer = () => {
-  console.log("정답입니다.");
+const rightAnswer = (userInput) => {
+  const answer = document.querySelector(".answer");
+  answer.innerText = "정답입니다.";
+  const word = document.querySelector(".word");
+  word.innerText = userInput;
 };
 
-const wrongAnswer = () => {
-  console.log("오답입니다.");
+const wrongAnswer = (lifeNumber) => {
+  const answer = document.querySelector(".answer");
+  const life = document.querySelector(".life");
+  if (lifeNumber === 0) {
+    const form = document.querySelector("form");
+    answer.innerText = "";
+    document.body.removeChild(form);
+    return (life.innerText = "탈락하셨습니다.");
+  }
+  answer.innerText = "오답입니다.";
+  life.innerText = `목숨 : ${lifeNumber}`;
 };
 
-const handleOnUserInput = (e) => {
+const handleOnUserInput = (life) => (e) => {
   e.preventDefault();
   const userInput = document.querySelector("input");
   const word = document.querySelector(".word");
   if (userInput.value.match(/[^가-힣]/g)) return alert("단어가 아닙니다.");
   word.innerText[word.innerText.length - 1] === userInput.value[0]
-    ? rightAnswer()
-    : wrongAnswer();
+    ? rightAnswer(userInput.value)
+    : wrongAnswer(--life);
 };
 
 const gameStart = () => {
   makeScreen();
+  let life = 5;
   const form = document.querySelector("form");
-  form.addEventListener("submit", handleOnUserInput);
+  form.addEventListener("submit", handleOnUserInput(life));
 };
 
 gameStart();
