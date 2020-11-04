c83428cd1cdcdeec19009b2966c604265d1373db
unknown <dudtjr913@naver.com>
2020-11-04 21:27:54 +0900

    update : 끝말잇기 로직 변경 <description> 마지막일줄 알았던 지난 커밋에 이어 다시 한 번 하는 이유는 기존에 작성한 코드로 진행을 하면 body의 모든 노드들을 지우기 때문에 다른 게임까지 다 지워버린다. 그래서 끝말잇기라는 class를 두고 그 class만 지우는 로직으로 변경했다. <issue-number>

--- WordRelay/WordRelay.js
+++ WordRelay/WordRelay.js
@@ -1,91 +1,95 @@
-const makeScreen = () => {
-  document.body.firstElementChild &&
-    document.body.removeChild(document.body.firstElementChild);
-  const title = document.createElement("div");
-  const wordTitle = document.createElement("span");
-  const firstWord = document.createElement("span");
-  const form = document.createElement("form");
-  const userInput = document.createElement("input");
-  const life = document.createElement("div");
-  const answer = document.createElement("div");
-  title.innerHTML = "<h1>끝말잇기</h1>";
-  wordTitle.innerText = "단어 : ";
-  firstWord.classList.add("word");
-  firstWord.innerText = "프론트엔드";
-  userInput.placeholder = "단어를 입력해주세요.";
-  userInput.type = "text";
-  life.classList.add("life");
-  life.innerText = "목숨 : 5";
-  document.body.append(title);
-  title.append(wordTitle);
-  title.append(firstWord);
-  document.body.append(form);
-  form.append(userInput);
-  document.body.append(life);
-  answer.classList.add("answer");
-  document.body.append(answer);
-};
+"use strict";
+{
+  const makeScreen = () => {
+    const wordRelayWrapper = document.createElement("section");
+    const title = document.createElement("div");
+    const wordTitle = document.createElement("span");
+    const firstWord = document.createElement("span");
+    const form = document.createElement("form");
+    const userInput = document.createElement("input");
+    const life = document.createElement("div");
+    const answer = document.createElement("div");
+    wordRelayWrapper.classList.add("wordRelay");
+    document.body.append(wordRelayWrapper);
+    title.innerHTML = "<h1>끝말잇기</h1>";
+    wordTitle.innerText = "단어 : ";
+    firstWord.classList.add("word");
+    firstWord.innerText = "프론트엔드";
+    userInput.placeholder = "단어를 입력해주세요.";
+    userInput.type = "text";
+    life.classList.add("life");
+    life.innerText = "목숨 : 5";
+    wordRelayWrapper.append(title);
+    title.append(wordTitle);
+    title.append(firstWord);
+    wordRelayWrapper.append(form);
+    form.append(userInput);
+    wordRelayWrapper.append(life);
+    answer.classList.add("answer");
+    wordRelayWrapper.append(answer);
+  };
 
-const cleanScreen = () => {
-  while (document.body.firstElementChild) {
-    document.body.removeChild(document.body.firstElementChild);
-  }
-  return gameStart();
-};
+  const cleanScreen = () => {
+    const wordRelayWrapper = document.querySelector(".wordRelay");
+    document.body.removeChild(wordRelayWrapper);
+    return gameStart();
+  };
 
-const rightAnswer = (userInput) => {
-  const answer = document.querySelector(".answer");
-  answer.innerText = "정답입니다.";
-  const word = document.querySelector(".word");
-  word.innerText = userInput;
-};
+  const rightAnswer = (userInput) => {
+    const answer = document.querySelector(".answer");
+    answer.innerText = "정답입니다.";
+    const word = document.querySelector(".word");
+    word.innerText = userInput;
+  };
 
-const reStart = () => {
-  const button = document.createElement("button");
-  button.type = "submit";
-  button.innerText = "다시하기";
-  document.body.append(button);
-  button.onclick = cleanScreen;
-};
+  const reStart = (wordRelayWrapper) => {
+    const button = document.createElement("button");
+    button.type = "submit";
+    button.innerText = "다시하기";
+    wordRelayWrapper.append(button);
+    button.onclick = cleanScreen;
+  };
 
-const wrongAnswer = (lifeNumber) => {
-  const answer = document.querySelector(".answer");
-  const life = document.querySelector(".life");
-  if (lifeNumber === 0) {
-    const form = document.querySelector("form");
-    answer.innerText = "";
-    document.body.removeChild(form);
-    life.innerText = "탈락하셨습니다.";
-    return reStart();
-  }
-  answer.innerText = "오답입니다.";
-  life.innerText = `목숨 : ${lifeNumber}`;
-};
+  const wrongAnswer = (lifeNumber, wordRelayWrapper) => {
+    const answer = wordRelayWrapper.querySelector(".answer");
+    const life = wordRelayWrapper.querySelector(".life");
+    if (lifeNumber === 0) {
+      const form = wordRelayWrapper.querySelector("form");
+      answer.innerText = "";
+      wordRelayWrapper.removeChild(form);
+      life.innerText = "탈락하셨습니다.";
+      return reStart(wordRelayWrapper);
+    }
+    answer.innerText = "오답입니다.";
+    life.innerText = `목숨 : ${lifeNumber}`;
+  };
 
-const handleOnUserInput = (life) => (e) => {
-  e.preventDefault();
-  const userInput = document.querySelector("input");
-  const word = document.querySelector(".word");
-  if (userInput.value.match(/[^가-힣]/g) || userInput.value === "")
-    return alert("단어가 아닙니다.");
-  word.innerText[word.innerText.length - 1] === userInput.value[0]
-    ? rightAnswer(userInput.value)
-    : wrongAnswer(--life);
-  userInput.value = "";
-};
+  const handleOnUserInput = (life, wordRelayWrapper) => (e) => {
+    e.preventDefault();
+    const userInput = wordRelayWrapper.querySelector("input");
+    const word = wordRelayWrapper.querySelector(".word");
+    if (userInput.value.match(/[^가-힣]/g) || userInput.value === "")
+      return alert("단어가 아닙니다.");
+    word.innerText[word.innerText.length - 1] === userInput.value[0]
+      ? rightAnswer(userInput.value)
+      : wrongAnswer(--life, wordRelayWrapper);
+    userInput.value = "";
+  };
 
-const gameStart = () => {
-  makeScreen();
-  const life = 5;
-  const form = document.querySelector("form");
-  form.addEventListener("submit", handleOnUserInput(life));
-};
+  const gameStart = () => {
+    makeScreen();
+    const wordRelayWrapper = document.querySelector(".wordRelay");
+    const life = 5;
+    const form = wordRelayWrapper.querySelector("form");
+    form.addEventListener("submit", handleOnUserInput(life, wordRelayWrapper));
+  };
 
-const gameStartButton = () => {
-  const button = document.createElement("button");
-  button.innerText = "끝말잇기 시작";
-  document.body.append(button);
-  button.addEventListener("click", gameStart);
-};
+  const gameStartButton = () => {
+    const button = document.createElement("button");
+    button.innerText = "끝말잇기 시작";
+    document.body.append(button);
+    button.addEventListener("click", gameStart);
+  };
 
-gameStartButton();
+  gameStartButton();
+}
