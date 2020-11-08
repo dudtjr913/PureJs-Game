299e89f22cb2165e6bd06ed97777b78be49040e5
unknown <dudtjr913@naver.com>
2020-11-08 15:41:39 +0900

    update : 로또 추첨기 - 숫자 45까지 화면에 띄우기 <description> <issue-number>

--- Lotto/Lotto.js
+++ Lotto/Lotto.js
@@ -18,4 +18,46 @@
       lottoBonusNumber,
     };
   };
+
+  const handleOnNumberClick = (e) => {
+    // 숫자 클릭하면 class를 붙여줌
+    e.target.classList.toggle("clicked");
+  };
+
+  const makeScreen = () => {
+    const lottoWrapper = document.createElement("section");
+    const title = document.createElement("h1");
+    const div = document.createElement("div");
+    const numbersUl = document.createElement("ul");
+    const form = document.createElement("form");
+    const button = document.createElement("button");
+
+    lottoWrapper.classList.add("lotto-wrapper");
+    title.innerText = "로또 추첨기";
+    div.innerText = "로또 추첨기입니다. 숫자 6자리를 골라주세요.";
+    button.type = "submit";
+    button.innerText = "제출하기";
+
+    document.body.appendChild(lottoWrapper);
+    lottoWrapper.appendChild(title);
+    lottoWrapper.appendChild(div);
+    lottoWrapper.appendChild(numbersUl);
+
+    for (let i = 0; i < 9; i++) {
+      // 1 ~ 45까지의 숫자 화면에 표시
+      const numbersList = document.createElement("li");
+      for (let j = 1; j <= 5; j++) {
+        const number = document.createElement("span");
+        number.innerText = j + 5 * i; // 1 ~ 45
+        number.addEventListener("click", handleOnNumberClick); // 숫자 클릭 시 콜백함수 호출
+        numbersList.appendChild(number);
+      }
+      numbersUl.appendChild(numbersList);
+    }
+
+    lottoWrapper.appendChild(form);
+    form.appendChild(button);
+  };
+
+  makeScreen();
 }
