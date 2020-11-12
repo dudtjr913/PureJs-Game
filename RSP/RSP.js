00bd76d41358e377ce1d7703c1ae39f74085e77d
unknown <dudtjr913@naver.com>
2020-11-12 10:12:39 +0900

    update : debounce 완료 <description> return의 의미를 완벽하게 파악할 수 있게 되었음 <issue-number>

--- RSP/RSP.js
+++ RSP/RSP.js
@@ -1,33 +1,33 @@
-"use strict";
+'use strict';
 {
   const changeRSP = (image) => {
     // 가위바위보 순서대로 출력하기 위해 className을 바꿔줌
     switch (image.className) {
-      case "rock":
-        image.classList.replace("rock", "scissors");
+      case 'rock':
+        image.classList.replace('rock', 'scissors');
         break;
-      case "scissors":
-        image.classList.replace("scissors", "paper");
+      case 'scissors':
+        image.classList.replace('scissors', 'paper');
         break;
-      case "paper":
-        image.classList.replace("paper", "rock");
+      case 'paper':
+        image.classList.replace('paper', 'rock');
         break;
     }
   };
 
   const makeScreen = () => {
-    const RSPWrapper = document.createElement("section");
-    const title = document.createElement("h1");
-    const imageWrapper = document.createElement("div");
-    const image = document.createElement("img");
+    const RSPWrapper = document.createElement('section');
+    const title = document.createElement('h1');
+    const imageWrapper = document.createElement('div');
+    const image = document.createElement('img');
 
-    RSPWrapper.classList.add("RSP-wrapper");
-    title.innerText = "가위바위보";
-    imageWrapper.style.maxWidth = "200px";
-    imageWrapper.style.overflow = "hidden";
+    RSPWrapper.classList.add('RSP-wrapper');
+    title.innerText = '가위바위보';
+    imageWrapper.style.maxWidth = '200px';
+    imageWrapper.style.overflow = 'hidden';
 
-    image.src = "RSP/RSP.jpeg";
-    image.classList.add("rock");
+    image.src = 'RSP/RSP.jpeg';
+    image.classList.add('rock');
 
     const changing = setInterval(changeRSP.bind(null, image), 1000); // 가위바위보 순서대로 화면에 출력
 
@@ -40,34 +40,32 @@
   };
 
   const cleanScreen = () => {
-    const RSPWrapper = document.querySelector(".RSP-wrapper");
+    const RSPWrapper = document.querySelector('.RSP-wrapper');
     document.body.removeChild(RSPWrapper);
     return gameStartButton();
   };
 
-  const debounce = (func, time, any) => {
+  const debounce = (func, time, interval) => {
+    const prevFunc = func(interval);
     let setTime = null;
-    let interval = any;
-    console.log(func);
     return (e) => {
-      clearTimeout(setTime);
-      setTime = setTimeout(func(e, interval), time);
+      setTime = setTimeout(prevFunc.bind(null, e), time);
     };
   };
 
-  const handleOnResult = (e, interval) => {
-    const RSPWrapper = document.querySelector(".RSP-wrapper");
-    const image = RSPWrapper.querySelector("img");
-    console.log(interval);
-    return () => {
+  const handleOnResult = (interval) => {
+    const RSPWrapper = document.querySelector('.RSP-wrapper');
+    const image = RSPWrapper.querySelector('img');
+    return (e) => {
+      console.log(interval);
       clearInterval(interval);
       interval = setInterval(changeRSP.bind(null, image), 1000);
     };
   };
 
   const userSelectButton = (intervalValue) => {
-    const RSPWrapper = document.querySelector(".RSP-wrapper");
-    const buttonWrapper = document.createElement("div");
+    const RSPWrapper = document.querySelector('.RSP-wrapper');
+    const buttonWrapper = document.createElement('div');
     buttonWrapper.innerHTML = `
     <div class="buttons-wrapper">
     <button class="rock-btn" type="submit">바위</button>
@@ -77,8 +75,8 @@
     `;
     RSPWrapper.appendChild(buttonWrapper);
 
-    const buttons = RSPWrapper.querySelector(".buttons-wrapper");
-    buttons.addEventListener("click", debounce(handleOnResult, 500, intervalValue));
+    const buttons = RSPWrapper.querySelector('.buttons-wrapper');
+    buttons.addEventListener('click', debounce(handleOnResult, 500, intervalValue));
   };
 
   const gameStart = () => {
@@ -88,22 +86,22 @@
   };
 
   const gameStartButton = () => {
-    let button = document.querySelector(".RSP-btn");
+    let button = document.querySelector('.RSP-btn');
     if (!button) {
-      button = document.createElement("button");
-      button.classList.add("RSP-btn");
+      button = document.createElement('button');
+      button.classList.add('RSP-btn');
       document.body.appendChild(button);
     }
-    button.innerText = "가위바위보 시작";
-    button.removeEventListener("click", cleanScreen);
-    button.addEventListener("click", gameStart);
+    button.innerText = '가위바위보 시작';
+    button.removeEventListener('click', cleanScreen);
+    button.addEventListener('click', gameStart);
   };
 
   const gameFinishButton = () => {
-    const button = document.querySelector(".RSP-btn");
-    button.innerText = "가위바위보 종료";
-    button.removeEventListener("click", gameStart);
-    button.addEventListener("click", cleanScreen);
+    const button = document.querySelector('.RSP-btn');
+    button.innerText = '가위바위보 종료';
+    button.removeEventListener('click', gameStart);
+    button.addEventListener('click', cleanScreen);
   };
 
   gameStartButton();
